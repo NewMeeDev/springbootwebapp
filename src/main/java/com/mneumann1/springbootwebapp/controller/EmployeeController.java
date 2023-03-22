aa0480c00e5f8d8ec2032af540e3e04e716223b3
Marc Neumann <isoldemaduschn@web.de>
2023-03-22 11:31:24 +0100

    final and german translation

--- src/main/java/com/mneumann1/springbootwebapp/controller/EmployeeController.java
+++ src/main/java/com/mneumann1/springbootwebapp/controller/EmployeeController.java
@@ -8,7 +8,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mneumann1.springbootwebapp.entity.Employee;
@@ -31,5 +33,34 @@ public class EmployeeController {
 		mav.addObject("employees", list);
 		return mav;
 	}
+	
+	@GetMapping("/addEmployeeForm")
+	public ModelAndView addEmployeeForm() {
+		ModelAndView mav = new ModelAndView("add-employee-form");
+		Employee newEmployee = new Employee();
+		mav.addObject("employee", newEmployee);
+		return mav;
+	}
+	
+	@PostMapping("/saveEmployee")
+	public String saveEmployee(@ModelAttribute Employee employee) {
+		eRepo.save(employee);
+		return "redirect:/list";
+	}
+	
+	@GetMapping("/showUpdateForm")
+	public ModelAndView showUpdateForm(@RequestParam Long employeeId) {
+		ModelAndView mav = new ModelAndView("add-employee-form");
+		Employee employee = eRepo.findById(employeeId).get();
+		mav.addObject("employee", employee);		
+		return mav;
+	}
+	
+	@GetMapping("/deleteEmployee")
+	public String deleteEmployee(@RequestParam Long employeeId) {
+		eRepo.deleteById(employeeId);
+		return "redirect:/list";
+	}
+	
 
 }
